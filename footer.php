b1bbde1faf974555cc23af7c26da779e5f248522
Mohd Hesam <75774130+HesaMtiPu@users.noreply.github.com>
2024-01-04 15:24:18 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
  <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Student Management System
+      Public DNS Management System
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2020.</strong> All rights reserved.
